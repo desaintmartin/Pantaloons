1336c42c1c62837dc861a88f379308dbee2eb323
Cédric de Saint Martin <cedric.desaintmartin@francetv.fr>
2015-04-23 18:33:37 +0200

    Add kolor eyes bat script.

---
+++ KolorEyes/camera1.bat
@@ -0,0 +1,5 @@
+@echo off
+
+set kolor="C:\Program Files (x86)\Kolor\KolorEyes 1.4\KolorEyes_win32.exe"
+
+start "Roland Garros 360 par France Télévisions" %kolor% "http://ftvodhdsecz-f.akamaihd.net/i/streaming-adaptatif/evt/pf-sport/2015/04/50397-1428499680-1-,1920x960-6272k,1920x960-4224k,1920x960-2176k,.mp4.csmil/master.m3u8"
